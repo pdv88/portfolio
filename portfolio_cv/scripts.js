11e15b294c0726b6c6a868d011f6a92e9edd39ee
Pablo De La Garza Vargas <54457168+pdv88@users.noreply.github.com>
2023-08-09 14:18:01 +0200

    first commit

---
+++ portfolio_cv/scripts.js
@@ -0,0 +1,28 @@
+// Funcion para añadir o quitar la clase slideIn a secciones
+function handleSectionIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    } else {
+      entry.target.classList.remove('slide-in');
+    }
+  });
+}
+
+// Configuracion del observador de interseccion
+const sectionObserverOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2, // The amount of section visible needed to trigger the animation
+};
+
+// Variable de interseccion de seccion
+const sectionObserver = new IntersectionObserver(handleSectionIntersect, sectionObserverOptions);
+
+// Variable de seleccion de secciones
+const sections = document.querySelectorAll('section');
+
+// Observing each section
+sections.forEach((section) => {
+  sectionObserver.observe(section);
+});
