c6e5a98914deb4a7c671782349559108a43c8ba2
Pablo De La Garza Vargas <54457168+pdv88@users.noreply.github.com>
2023-10-17 18:36:44 +0200

    add Experience section css, html and js fix some responsive issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,52 @@
-// Funcion para añadir o quitar la clase slide-in a secciones
-function handleSectionIntersect(entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      setTimeout(function(){entry.target.classList.add('slide-in')}, 500)
-    } 
-  });
+// ----------------funcion activar link  ---------------------
+
+let navLinks = document.getElementsByClassName('nav_link');
+let burgerNavLinks = document.getElementsByClassName('nav_link_burger');
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', activeLink)
+  burgerNavLinks[i].addEventListener('click', activeLink)
+}
+function activeLink(e) {
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.remove('activeLink');
+    burgerNavLinks[i].classList.remove('activeLink');
+  }
+  e.target.classList.add('activeLink');
 }
 
-// Configuracion del observador de interseccion
-const sectionObserverOptions = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.2, // The amount of section visible needed to trigger the animation
-};
+// --------- funcion activar link con scroll---------------
 
-// Variable de interseccion de seccion
-const sectionObserver = new IntersectionObserver(handleSectionIntersect, sectionObserverOptions);
-
-// Variable de seleccion de secciones
-const sections = document.querySelectorAll('section');
-
-// Observing each section
-sections.forEach((section) => {
-  sectionObserver.observe(section);
+let sections = document.querySelectorAll('.mainSection');
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      let id = '#' + entry.target.id;
+			history.pushState({}, entry.target.innetText, id);
+      for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].classList.remove('activeLink');
+        let href = navLinks[i].attributes.href.value;
+        if (href === id) {
+          navLinks[i].classList.add('activeLink');
+        }
+      }
+      for (let i = 0; i < burgerNavLinks.length; i++) {
+        burgerNavLinks[i].classList.remove('activeLink');
+        let href = burgerNavLinks[i].attributes.href.value;
+        if (href === id) {
+          burgerNavLinks[i].classList.add('activeLink');
+        }
+      }
+		}
+	});
+}, {
+  threshold: 1,
+	rootMargin: '0px 0px 30% 0px'
 });
+sections.forEach(section => {
+  observer.observe(section);
+})
 
-// Menu hamburguesa
+// -----------------Menu hamburguesa-----------------------
 
 const burger = document.getElementById('burger-icon');
 const modal = document.getElementById('burger-modal');
@@ -39,3 +60,27 @@ modal.addEventListener("click", () =>{
   modal.style.opacity = "0";
   modal.style.visibility = "hidden";
 });
+
+// -----------------Tabs Experiencia-----------------------
+
+let buttonTabs = document.getElementsByClassName('tab');
+let contentTabs = document.getElementsByClassName('tab_content');
+  for (let i = 0; i < buttonTabs.length; i++) {
+    buttonTabs[i].addEventListener('click', showTab);        
+  }
+
+contentTabs[0].style.display = 'flex';
+buttonTabs[0].classList.add('active');
+
+function showTab(e) {
+  
+  for (let i = 0; i < contentTabs.length; i++) {
+    if (contentTabs[i].firstChild.nextSibling.innerHTML == e.target.innerHTML) {
+      contentTabs[i].style.display = 'flex';
+      buttonTabs[i].classList.add('active');
+    } else {
+      contentTabs[i].style.display = 'none'
+      buttonTabs[i].classList.remove('active');
+    }
+  }
+}
